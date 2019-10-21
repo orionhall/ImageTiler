6416f23eaa69e3232e9a20778918a170312644f3
Orion Hall <orion.hall@ert.com>
2019-10-21 18:51:51 -0400

    Removed state from TilerBody and added it to ImageTiler so it can interact with settings button/modal

--- src/components/TilerBody/TilerBody.js
+++ src/components/TilerBody/TilerBody.js
@@ -1,73 +1,32 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
 import TileRow from '../TileRow/TileRow';
-import { getFlickrData, makePhotoURL } from '../../actions/getFlickrData';
 import '../../ImageTiler.css';
 
-class TilerBody extends React.Component {
-    
-    constructor (props) {
-        super(props);
-
-        this.state = {
-            photos: null,
-            photosPerRow: 3
-        };
-    };
-
-    componentDidMount () {
-        getFlickrData().then((response) => {
-            let photos = response.photos ? response.photos : response.items;
-            photos = photos.map((photo, index) => {
-                let newPhotoObj = {...photo},
-                    photoNumber = index + 1,
-                    rowNumber = photoNumber % this.state.photosPerRow !== 0 ?
-                        Math.floor(photoNumber / this.state.photosPerRow) :
-                        (photoNumber / this.state.photosPerRow - 1);
-                newPhotoObj.rowNumber = rowNumber;
-                newPhotoObj.src = makePhotoURL(photo);
-                return newPhotoObj;
-            });
-            this.setState({ photos });
+const TilerBody = (props) => {
+    let formattedPhotos = [];
+    const { photos } = props;
+    if (photos) {
+        photos.forEach((photo) => {
+            if (formattedPhotos[photo.rowNumber]) {
+                formattedPhotos[photo.rowNumber].push(photo);
+            } else {
+                formattedPhotos.push([ photo ]);
+            }
         });
     }
-    
-    render () {
-        let formattedPhotos = [];
-        if (this.state.photos) {
-            this.state.photos.forEach((photo) => {
-                if (formattedPhotos[photo.rowNumber]) {
-                    formattedPhotos[photo.rowNumber].push(photo);
-                } else {
-                    formattedPhotos.push([ photo ]);
-                }
-            });
-            console.log(formattedPhotos);
-            // const numPhotos = this.state.photos.length,
-            //     photosPerRow = this.state.photosPerRow,
-            //     rowsNeeded = numPhotos % photosPerRow === 0 ? numPhotos / photosPerRow : Math.floor(numPhotos / photosPerRow ) + 1;
-            // for (let i = 1; i <= rowsNeeded; i++) {
-            //     const maxThisRow = (i * photosPerRow) - 1, // row 1 = 2, row 2 = 5
-            //         minThisRow = maxThisRow - photosPerRow, // row 1 = 0, row 2 = 3
-            //         thisRowPhotos = this.state.photos.filter((photo, index) => {
-            //             return photo.id >= minThisRow && photo.id <= maxThisRow;
-            //         });
-            //     formattedPhotos.push(thisRowPhotos);
-            // }
-        }
 
-        return (
-            <Aux>
-                <div className="tilerBody">
-                    {formattedPhotos.map((rowPhotos, index) => {
-                        return (
-                            <TileRow key={index} rowPhotos={rowPhotos} />
-                        )
-                    })}
-                </div>
-            </Aux>
-        );
-    }; 
-}
+    return (
+        <Aux>
+            <div className="tilerBody">
+                {formattedPhotos.map((rowPhotos, index) => {
+                    return (
+                        <TileRow key={index} rowPhotos={rowPhotos} />
+                    )
+                })}
+            </div>
+        </Aux>
+    );
+};
 
 export default TilerBody;
--- src/containers/ImageTiler/ImageTiler.js
+++ src/containers/ImageTiler/ImageTiler.js
@@ -2,17 +2,55 @@ import React from 'react';
 import Aux from '../../hoc/Auxiliary';
 import TilerBody from '../../components/TilerBody/TilerBody';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SettingsModal from '../../components/SettingsModal/SettingsModal';
+import { getFlickrData, makePhotoURL } from '../../actions/getFlickrData';
 
-const ImageTiler = (props) => {
-    return (
-        <Aux>
-            {/* Navbar? */}
-            <Toolbar></Toolbar>
-            <main className="content">
-                <TilerBody />
-            </main>
-        </Aux>
-    );
+class ImageTiler extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            photos: null,
+            photosPerRow: 3,
+            showSettingsModal: false
+        };
+    };
+
+    componentDidMount() {
+        getFlickrData().then((response) => {
+            let photos = response.photos ? response.photos : response.items;
+            photos = photos.map((photo, index) => {
+                let newPhotoObj = {...photo},
+                    photoNumber = index + 1,
+                    rowNumber = photoNumber % this.state.photosPerRow !== 0 ?
+                        Math.floor(photoNumber / this.state.photosPerRow) :
+                        (photoNumber / this.state.photosPerRow - 1);
+                newPhotoObj.rowNumber = rowNumber;
+                newPhotoObj.src = makePhotoURL(photo);
+                return newPhotoObj;
+            });
+            this.setState({ photos });
+        });
+    }
+
+    toggleSettingsModal() {
+        let showSettingsModal = this.state.showSettingsModal === false ? true : false;
+        this.setState({ showSettingsModal });
+        console.log('Clicked!');
+        console.log(this.state.showSettingsModal);
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar toggleModalHandler={this.toggleSettingsModal.bind(this)} />
+                <main className="content">
+                    <TilerBody photos={this.state.photos} />
+                </main>
+                <SettingsModal show={this.state.show}/>
+            </Aux>
+        ); 
+    };
 };
 
 export default ImageTiler;
